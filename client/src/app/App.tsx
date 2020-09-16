acf657ed4b2e73e45c7407da1d62345073003787
svyatoslav <svyatoslavtt>
2020-09-16 18:04:35 +0300

    client: Add new routes and global NavBar

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import {Switch, Route, Redirect} from 'react-router-dom';
 
-import {AuthRoutes, ContentRoutes} from "./routes/routes-const";
+import {AuthRoutes, AppRoutes} from "./routes/routes-const";
 import NewsFeed from "./pages/NewsFeed";
 import SignIn from "./pages/Auth/SignIn";
+import SignUp from "./pages/Auth/SignUp";
+import NavBar from "./shared/components/NavBar";
 
 const App: React.FC = () => {
   return (
-    <Switch>
-      <Route exact path={ContentRoutes.NEWS_FEED} component={NewsFeed} />
-      <Route exact path={AuthRoutes.SIGN_IN} component={SignIn} />
-      <Redirect to={ContentRoutes.NEWS_FEED} />
-    </Switch>
+    <>
+      <NavBar />
+      <Switch>
+        <Route exact path={AppRoutes.NEWS_FEED} component={NewsFeed} />
+        <Route exact path={AuthRoutes.SIGN_IN} component={SignIn} />
+        <Route exact path={AuthRoutes.SIGN_UP} component={SignUp} />
+        <Redirect to={AppRoutes.NEWS_FEED} />
+      </Switch>
+    </>
   )
 }
 
