2133793cee7c9e10a64f35c0c4fb58e5c051bc47
svyatoslav <svyatoslavtt>
2020-09-16 19:05:47 +0300

    server: Add User module

---
+++ server/models/User.js
@@ -0,0 +1,33 @@
+const { model, Schema, Types } = require("mongoose");
+
+const schema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  confirmPassword: {
+    type: String,
+    required: true,
+  },
+  films: [
+    {
+      type: Types.ObjectId,
+      ref: 'Film',
+    },
+  ],
+});
+
+module.exports = model('User', schema);
