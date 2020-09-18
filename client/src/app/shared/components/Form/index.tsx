83dab67bfd898f16e3ecf3bc821566e16031c047
svyatoslav <svyatoslavtt>
2020-09-18 19:42:06 +0300

    client: Refactor Form component

--- client/src/app/shared/components/Form/index.tsx
+++ client/src/app/shared/components/Form/index.tsx
@@ -1,32 +1,22 @@
 import React from "react";
+import styles from "./style.module.scss";
 import {NavLink} from "react-router-dom";
-
-import styles from "./style.module.scss"
-
-import Button from "../Button";
-import {Input} from "../../interfaces/input.interface";
-
 import {AuthRoutes} from "../../../routes/routes-const";
+import Button from "../Button";
 
 interface FormProps {
-  inputs: Input[];
+  handleSubmit: () => void;
+  form: any;
+  setForm: any;
   title: string;
 }
 
-const Form: React.FC<FormProps> = ({ inputs, title }) => {
-  const signUp = title === 'Вход';
+const Form: React.FC<FormProps> = ({ handleSubmit, setForm, form, title }) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({ ...form, [event.target.name]: event.target.value });
+  }
 
-  const renderInputs =
-    Object.values(inputs)
-      .map((item: Input) => (
-        <input
-          key={item.id}
-          type={item.type}
-          placeholder={item.placeholder}
-          name={item.name}
-          required={item.required}
-        />
-      ));
+  const login = title === 'Вход';
 
   return (
     <div className={styles.container}>
@@ -35,21 +25,76 @@ const Form: React.FC<FormProps> = ({ inputs, title }) => {
           {title}
         </div>
 
-        {signUp && (
+        {login && (
           <NavLink to={AuthRoutes.SIGN_UP} className={styles.signUp}>
             Регистрация
           </NavLink>
         )}
-
       </div>
 
       <div className={styles.formWrapper}>
-        <form method="POST" className={styles.form}>
+        <div className={styles.form}>
           <div className={styles.inputWrapper}>
-            {renderInputs}
+            {login ? (
+              <>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  value={form.email}
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  name="password"
+                  value={form.password}
+                  placeholder="Password"
+                  onChange={handleChange}
+                />
+              </>
+            ) : (
+              <>
+                <input
+                  type="text"
+                  name="email"
+                  placeholder="Email"
+                  value={form.email}
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  name="name"
+                  value={form.name}
+                  placeholder="Name"
+                  onChange={handleChange}
+                />
+                <input
+                  type="text"
+                  name="surname"
+                  value={form.surname}
+                  placeholder="Surname"
+                  onChange={handleChange}
+                />
+                <input
+                  type="password"
+                  name="password"
+                  value={form.password}
+                  placeholder="Password"
+                  onChange={handleChange}
+                />
+                <input
+                  type="password"
+                  name="confirmPassword"
+                  value={form.confirmPassword}
+                  placeholder="Confirm password"
+                  onChange={handleChange}
+                />
+              </>
+            )}
           </div>
-          <Button text={title} />
-        </form>
+        </div>
+
+        <Button text={login ? 'Войти' : 'Готово'} onClick={handleSubmit} />
       </div>
     </div>
   )
