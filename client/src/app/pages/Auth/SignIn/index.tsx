0d794f135fb07a10aa90231c4fb7b93b05629e8a
svyatoslav <svyatoslavtt>
2020-09-18 19:41:31 +0300

    client: Refactor SignIn component

--- client/src/app/pages/Auth/SignIn/index.tsx
+++ client/src/app/pages/Auth/SignIn/index.tsx
@@ -1,28 +1,25 @@
-import React from "react";
-
+import React, {useCallback, useState} from "react";
+import {useDispatch} from "react-redux";
+import {Login} from "../../../shared/interfaces/auth.interface";
+import {Actions} from "../../../redux/auth/action";
 import Form from "../../../shared/components/Form";
-import {Input} from "../../../shared/interfaces/input.interface";
 
 const SignIn: React.FC = () => {
-  const inputs: Input[] = [
-    {
-      id: 1,
-      type: 'text',
-      placeholder: 'Email',
-      name: 'email',
-      required: true,
-    },
-    {
-      id: 2,
-      type: 'password',
-      placeholder: 'Password',
-      name: 'password',
-      required: true,
-    }
-  ];
+  const dispatch = useDispatch();
+
+  const initialFormData: Login = {
+    email: '',
+    password: '',
+  };
+
+  const [form, setForm] = useState(initialFormData);
+
+  const handleSubmit = useCallback(() => {
+    dispatch(Actions.loginRequest({...form}));
+  }, [form, dispatch]);
 
   return (
-    <Form inputs={inputs} title="Вход" />
+    <Form setForm={setForm} form={form} handleSubmit={handleSubmit} title='Вход' />
   )
 }
 
