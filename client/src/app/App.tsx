f4f7bca01d28cba3872b67787e2e7b87814a44ac
svyatoslav <svyatoslavtt>
2020-09-19 16:26:46 +0300

    client: Add PrivateRoute fot auth

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -6,15 +6,16 @@ import NewsFeed from "./pages/NewsFeed";
 import SignIn from "./pages/Auth/SignIn";
 import SignUp from "./pages/Auth/SignUp";
 import NavBar from "./shared/components/NavBar";
+import PrivateRoute from "./shared/components/AuthPrivateRoute";
 
 const App: React.FC = () => {
   return (
     <>
       <NavBar />
       <Switch>
-        <Route exact path={AppRoutes.NEWS_FEED} component={NewsFeed} />
-        <Route exact path={AuthRoutes.SIGN_IN} component={SignIn} />
-        <Route exact path={AuthRoutes.SIGN_UP} component={SignUp} />
+        <Route exact={true} path={AppRoutes.NEWS_FEED} component={NewsFeed} />
+        <PrivateRoute exact={true} path={AuthRoutes.SIGN_IN} component={SignIn} />
+        <PrivateRoute exact={true} path={AuthRoutes.SIGN_UP} component={SignUp} />
         <Redirect to={AppRoutes.NEWS_FEED} />
       </Switch>
     </>
