b60ddd36aa55d56df4a630389af92e3ec73f1a9f
svyatoslav <svyatoslavtt>
2020-09-19 12:53:58 +0300

    client: Update initial data for inputs

--- client/src/app/pages/Auth/SignIn/inputs-data.ts
+++ client/src/app/pages/Auth/SignIn/inputs-data.ts
@@ -1,18 +1,22 @@
 import {InputInterface} from "../../../shared/interfaces/input.interface";
 
-export const inputs: InputInterface[] = [
-  {
+export const inputs = {
+  email: {
     id: 1,
     type: 'text',
     placeholder: 'Email',
     name: 'email',
+    value: '',
+    empty: true,
     required: true,
   },
-  {
+  password: {
     id: 2,
     type: 'password',
     placeholder: 'Password',
     name: 'password',
+    value: '',
+    empty: true,
     required: true,
   }
-];
+};
--- client/src/app/pages/Auth/SignUp/inputs-data.ts
+++ client/src/app/pages/Auth/SignUp/inputs-data.ts
@@ -1,38 +1,47 @@
 import {InputInterface} from "../../../shared/interfaces/input.interface";
 
-export const inputs: InputInterface[] = [
-  {
+export const inputs: { [key: string]: InputInterface } = {
+  email: {
     id: 1,
     type: 'text',
     placeholder: 'Email',
     name: 'email',
+    value: '',
+    empty: false,
     required: true,
   },
-  {
+  name: {
     id: 2,
     type: 'text',
     placeholder: 'Name',
     name: 'name',
+    value: '',
+    empty: false,
     required: true,
   },
-  {
+  surname: {
     id: 3,
     type: 'text',
     placeholder: 'Surname',
     name: 'surname',
+    value: '',
   },
-  {
+  password: {
     id: 4,
     type: 'password',
     placeholder: 'Password',
     name: 'password',
+    value: '',
+    empty: false,
     required: true,
   },
-  {
+  confirmPassword: {
     id: 5,
     type: 'password',
     placeholder: 'Confirm password',
     name: 'confirmPassword',
+    value: '',
+    empty: false,
     required: true,
   },
-];
+};
