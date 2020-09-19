1c1d31507fafc928473e61f17edd88713f63d32c
svyatoslav <svyatoslavtt>
2020-09-19 12:53:21 +0300

    client: Create Input component

---
+++ client/src/app/shared/components/Input/index.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import styles from './style.module.scss';
+
+interface InputProps {
+  type: string;
+  name: string;
+  placeholder: string;
+  value: string;
+  empty: boolean | undefined;
+  setForm: any;
+  form: any;
+  required: boolean | undefined;
+}
+
+const Input: React.FC<InputProps> = ({
+  type,
+  name,
+  placeholder,
+  value,
+  empty,
+  required,
+  setForm,
+  form,
+}) => {
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setForm({
+      ...form,
+      [event.target.name]: {
+        ...form[event.target.name],
+        value: event.target.value
+      }
+    });
+  }
+
+  return (
+    <input
+      type={type}
+      name={name}
+      placeholder={placeholder}
+      value={form.value}
+      required={required}
+      className={empty ? styles.noValid : styles.valid}
+      onChange={handleChange}
+    />
+  );
+}
+
+export default Input;
