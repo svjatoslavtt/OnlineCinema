5614a8f49c805171ee06a98e6ea9f112acad606b
svyatoslav <svyatoslavtt>
2020-09-25 14:21:23 +0300

    client: Add UPLOAD_FILM route

--- client/src/app/routes/routes-const.ts
+++ client/src/app/routes/routes-const.ts
@@ -1,7 +1,8 @@
 export enum AppRoutes {
     NEWS_FEED = '/news-feed',
-    MY_OFFICE = '/my-office',
-    DETAILED_FILM = '/detailed-film'
+		MY_OFFICE = '/my-office',
+		DETAILED_FILM = '/detailed-film',
+		UPLOAD_FILM = '/my-office/upload-film',
 }
 
 export enum AuthRoutes {
