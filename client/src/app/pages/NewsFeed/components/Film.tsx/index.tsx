fa674fadf7018c21dec11c94247d100495146375
svyatoslav <svyatoslavtt>
2020-09-25 14:22:08 +0300

    client: Create Film component

---
+++ client/src/app/pages/NewsFeed/components/Film.tsx/index.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import Rating from '@material-ui/lab/Rating';
+
+import styles from './style.module.scss';
+
+import image from '../../../../static/images/film.jpeg';
+
+const Film: React.FC = () => {
+	const [value, setValue] = useState<number | null>(2.5);
+
+	return (
+		<div className={styles.filmContainer}>
+			<div className={styles.filmImage}>
+				<img src={image} alt="movie"/>
+			</div>
+
+			<div className={styles.filmData}>
+				<div className={styles.filmTitle}>Место под соснами</div>
+				<div className={styles.filmRating}>
+					<Rating
+						name="read-only"
+						value={value}
+						readOnly
+					/>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default Film;
