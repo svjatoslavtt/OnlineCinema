0e5c7bd8bf4ba8ed69c10be7ae6f0ce7431141fb
svyatoslav <svyatoslavtt>
2020-09-25 14:23:20 +0300

    client: Create Categorie component

---
+++ client/src/app/pages/MyOffice/components/Categorie/index.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import styles from './style.module.scss';
+
+import Film from '../../../NewsFeed/components/Film.tsx';
+import { NavLink } from 'react-router-dom';
+import App from '../../../../App';
+import { AppRoutes } from '../../../../routes/routes-const';
+
+interface CategorieProps {
+	title: string;
+}
+
+const Categorie: React.FC<CategorieProps> = ({ title }) => {
+	return (
+		<div className={styles.categorie}>
+			<div className={styles.categorieHeaderWrapper}>
+				<span className={styles.categorieTitle}>{title}</span>
+
+				{title === 'Мои фильмы' && (
+					<NavLink to={AppRoutes.UPLOAD_FILM} className={styles.categorieAddNewFilm}>
+						Загрузить новый фильм
+					</NavLink>
+				)}
+			</div>
+
+			<div className={styles.categorieFilmsWrapper}>
+				<Film />
+			</div>
+		</div>
+	)
+}
+
+export default Categorie;
