6f7bde5b5a5828fba987d11fcdac79598c5142c0
svyatoslav <svyatoslavtt>
2020-10-01 16:39:35 +0300

    client: And button types, add minor fix

--- client/src/app/shared/interfaces/film-upload.interface.ts
+++ client/src/app/shared/interfaces/film-upload.interface.ts
@@ -1,17 +1,6 @@
 import { History } from 'history';
 
-export interface FilmUpload {
-	image: string | null | ArrayBuffer;
-	title: string;
-	description: string;
-	likes: number;
-	rating: number | null;
-	userId: string;
-};
-
-export type FilmUploadOmit = Omit<FilmUpload, 'likes'>;
-
 export interface FilmUploadRequest {
-	film: FilmUploadOmit;
+	formData: FormData;
 	history: History;
 }
