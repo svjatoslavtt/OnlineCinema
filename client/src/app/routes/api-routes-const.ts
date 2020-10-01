965f190c1ba655b5e98f575cabb1bfee6ef3f1bd
svyatoslav <svyatoslavtt>
2020-10-01 14:53:49 +0300

    client: Add  GET_FILMS route

--- client/src/app/routes/api-routes-const.ts
+++ client/src/app/routes/api-routes-const.ts
@@ -1,6 +1,7 @@
 export enum ApiEndPoints {
   LOGIN = '/api/auth/login',
-	REGISTER = 'api/auth/register',
+	REGISTER = '/api/auth/register',
 	
+	GET_FILMS = '/api/film/news-feed',
 	FILM_UPLOAD = '/api/film/upload',
 }
