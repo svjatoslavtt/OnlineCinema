7d8923a69d0401c29db4c555a75d0eedc867ca40
svyatoslav <svyatoslavtt>
2020-10-01 16:40:34 +0300

    client: Add dynamic output films

--- client/src/app/pages/NewsFeed/index.tsx
+++ client/src/app/pages/NewsFeed/index.tsx
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Actions } from "../../redux/get-films/action";
+import { getFilms } from "../../redux/get-films/selectors";
 
 import Film from "./components/Film.tsx";
 import styles from "./style.module.scss";
 
 const NewsFeed: React.FC = () => {
+	const dispatch = useDispatch();
+	const films = useSelector(getFilms);
+
+	useEffect(() => {
+		dispatch(Actions.getFilmsRequest());
+	}, [dispatch]);
+
   return (
     <div className={styles.newsFeedContainer}>
-      <Film />
+			{films && 
+				films.length && 
+					films.map(({ id, title, rating, image }) => {
+						return (
+							<Film 
+								key={id}
+								title={title}
+								rating={rating}
+								image={image}
+						/>
+						)
+					})
+			}
     </div>
   )
 }
