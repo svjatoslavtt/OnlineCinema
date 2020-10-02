5fcdc8b3989d0fac76a9e31b0ce377ccab21d2a9
svyatoslav <svyatoslavtt>
2020-10-02 19:42:17 +0300

    client: Add redirect to Detailed film

--- client/src/app/pages/NewsFeed/components/Film.tsx/index.tsx
+++ client/src/app/pages/NewsFeed/components/Film.tsx/index.tsx
@@ -1,33 +1,44 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Rating from '@material-ui/lab/Rating';
 
 import styles from './style.module.scss';
+import { useHistory } from 'react-router-dom';
+import { AppRoutes } from '../../../../routes/routes-const';
 
-type FilmProps = {
+type FilmTypes = {
 	title: string;
 	rating: number;
 	image: string;
+	id: string;
 }
 
-const Film: React.FC<FilmProps> = ({ title, rating, image }) => (
-	<div className={styles.filmContainer}>
-		<div className={styles.imageContainer}>
-			<div className={styles.filmImage}>
-				<img src={image} alt="movie"/>
-			</div>
-		</div>
+const Film: React.FC<FilmTypes> = ({ title, rating, image, id }) => {
+	const history = useHistory();
 
-		<div className={styles.filmData}>
-			<div className={styles.filmTitle}>{title}</div>
-			<div className={styles.filmRating}>
-				<Rating
-					name="read-only"
-					value={rating}
-					readOnly
-				/>
+	const handlerDedailedRedirect = () => {
+		history.push(AppRoutes.FILM_DETAILED + '/' + id);
+	};
+
+	return (
+		<div className={styles.filmContainer} onClick={handlerDedailedRedirect}>
+			<div className={styles.imageContainer}>
+				<div className={styles.filmImage}>
+					<img src={image} alt="movie"/>
+				</div>
+			</div>
+
+			<div className={styles.filmData}>
+				<div className={styles.filmTitle}>{title}</div>
+				<div className={styles.filmRating}>
+					<Rating
+						name="read-only"
+						value={rating}
+						readOnly
+					/>
+				</div>
 			</div>
 		</div>
-	</div>
-);
+	);
+};
 
 export default Film;
