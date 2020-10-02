9f30ecc3244df3460769a762e0079638e96c564e
svyatoslav <svyatoslavtt>
2020-10-02 19:40:55 +0300

    client: Add get data for MyOffice

--- client/src/app/pages/MyOffice/index.tsx
+++ client/src/app/pages/MyOffice/index.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import styles from './style.module.scss';
 import Categorie from './components/Categorie';
+import { useDispatch, useSelector } from 'react-redux';
+import { Actions } from '../../redux/get-films/action';
+import { getMyFilms } from '../../redux/get-films/selectors';
 
 const MyOffice: React.FC = () => {
+	const dispatch = useDispatch();
+	const films = useSelector(getMyFilms);
+
+	useEffect(() => {
+		dispatch(Actions.getMyFilmsRequest());
+		dispatch(Actions.getMyLikesRequest());
+	}, [dispatch]);
+
 	return (
 		<div className={styles.myOfficeContainer}>
-			<Categorie title='Мои фильмы' />
+			<Categorie title='Мои фильмы' data={films} />
 			<Categorie title='Мои лайки' />
 		</div>
 	)
